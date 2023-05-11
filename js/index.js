2c3605da53688e6b5c5faf54890ba1387311c62d
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2023-05-11 15:12:57 +0200

    ex fin

--- js/index.js
+++ js/index.js
@@ -13,3 +13,58 @@ let total = teamTotal + victory;
 let answer = "Tim & Jane now have " + total + " marbles!";
 
 console.log(answer)
+
+
+let timmyAmount = 10; 
+let susanAmount = 25; 
+
+let timmyTaxRate = 0.15; 
+let susanTaxRate = 0.26; 
+
+
+let timmyTaxAmount = timmyAmount * timmyTaxRate;
+let susanTaxAmount = susanAmount * susanTaxRate;
+
+let totalTaxAmount = timmyTaxAmount + susanTaxAmount;
+
+console.log(`The Tax Man will claim R${totalTaxAmount.toFixed(2)} from Timmy & Susan.`);
+
+
+
+let johnScore = 0;
+let timScore = 0;
+let alfieScore = 0;
+let gwenScore = 0;
+let totalScore = 0;
+let averageScore = 0;
+
+
+johnScore = 67;
+timScore = 80;
+alfieScore = 90;
+gwenScore = 56;
+
+
+totalScore = johnScore + timScore + alfieScore + gwenScore;
+
+
+averageScore = totalScore / 4;
+
+
+console.log(`The students' average score before adding 7 marks is: ${averageScore/100}`);
+
+
+johnScore += 7;
+timScore += 7;
+alfieScore += 7;
+gwenScore += 7;
+
+
+totalScore = johnScore + timScore + alfieScore + gwenScore;
+
+
+averageScore = totalScore / 4;
+
+
+console.log(`The students' average score after adding 7 marks is: ${averageScore/100}`);
+
